84ac14432f657cd02a37a9713f85ecbf95a54d84
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-07-20 11:24:03 -0300

    .

--- src/Componentes/Botao/index.js
+++
@@ -1,9 +0,0 @@
-import './Botao.css'
-
-function Botao(props){
-    return(
-        <button onClick={props.fecharModal} className="botao">Adicionar Tarefa</button>
-    )
-}
-
-export default Botao
