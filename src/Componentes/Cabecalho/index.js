2235ea87ee6f2fb6247e584b771f4a4c9f4ef87a
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-07-21 10:25:21 -0300

    .

--- src/Componentes/Cabecalho/index.js
+++ src/Componentes/Cabecalho/index.js
@@ -6,7 +6,7 @@ import { IoMdCreate } from 'react-icons/io';
 function Cabecalho(props){
     return(
         <header className='cabecalho'>
-            <h1><FcTodoList size={35} /> Minha lista de tarefas</h1>
+            <h1><FcTodoList size={30} /> Minha lista de tarefas</h1>
             <button onClick={props.abrirModal}><IoMdCreate />Criar tarefa</button>
         </header>
     )
