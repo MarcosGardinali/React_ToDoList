84278f691859fa9b5af1f1742c4c1cb14a3a0f20
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-07-26 10:09:24 -0300

    New

---
+++ src/Componentes/Lista/index.js
@@ -0,0 +1,26 @@
+import NenhumaTarefa from '../NenhumaTarefa'
+import Tarefas from '../Tarefas'
+import './Lista.css'
+
+function Lista(props){
+    return(
+        (props.tarefas.length > 0) ? <main className='lista'>
+            <h3>Suas Tarefas</h3>
+            <section className="tarefas-container">
+                {props.tarefas.map( tarefa => {
+                    return (<Tarefas
+                        id={tarefa.id} 
+                        key={tarefa.id} 
+                        nome={tarefa.nome} 
+                        check={tarefa.check}
+                        aoDeletar={props.aoDeletar}
+                        />
+                    )
+                })}
+            </section>    
+        </main>
+        : <NenhumaTarefa />
+    )
+}
+
+export default Lista
